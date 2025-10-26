f8279c00896f382541ed735959bd73b6d15665b3
Dmitry <Lostghost23@yandex.ru>
2025-10-26 15:48:21 +0300

    feat: С алишером

--- src/infrastructure/db/di/SessionProvider.py
+++ src/infrastructure/db/di/SessionProvider.py
@@ -13,7 +13,7 @@ class SessionProvider(Provider):
         postgres_dsn: URL = URL.create(
             drivername="postgresql+asyncpg",
             username=os.getenv("POSTGRES_USER"),
-            password=os.getenv("POSTGRES_PASS"),
+            password=os.getenv("POSTGRES_PASSWORD"),
             host=os.getenv("POSTGRES_HOST"),
             port=os.getenv("POSTGRES_PORT"),
             database=os.getenv("POSTGRES_DATABASE"),
