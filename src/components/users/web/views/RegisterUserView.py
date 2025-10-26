aeb218368eacdc94240749fc58f1b1fd70e78dac
Dmitry <Lostghost23@yandex.ru>
2025-10-26 15:55:26 +0300

    фит: с алишером

--- src/components/users/web/views/RegisterUserView.py
+++ src/components/users/web/views/RegisterUserView.py
@@ -17,21 +17,8 @@ class RegisterUserView:
         user_create: UserCreateRequest,
         user_service: FromDishka[IUserService],
         auth_service: FromDishka[IAuthService],
-    ):
-        try:
-            user = await user_service.register(user_create)
-            access_token = auth_service.create_access_token(subject=user.id)
-            
-            user_response = UserResponse.model_validate(user)
-            
-            response_data = {
-                "user": user_response.model_dump(),
-                "access_token": access_token,
-                "token_type": "bearer",
-            }
-            return JSONResponse(
-                status_code=status.HTTP_201_CREATED,
-                content=response_data
-            )
-        except UserAlreadyExistsException as e:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    ) -> UserResponse:
+        user = await user_service.register(user_create)
+        token = auth_service.create_token(user.email)
+        user.token = token
+        return UserResponse.model_validate(user)
